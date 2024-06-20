25f9b0ea1cfdfa2323b485b100d70cd73e58ebba
David Ayme <dayme1937@uta.edu.ec>
2024-06-19 23:40:48 -0500

    chore: Remove unused import statement in JsonAnalysisLibrary class

--- src/main/java/com/ayme/david/patrones/clase/estructurales/adapter/guru/JsonAnalysisLibrary.java
+++ src/main/java/com/ayme/david/patrones/clase/estructurales/adapter/guru/JsonAnalysisLibrary.java
@@ -1,14 +1,13 @@
 package com.ayme.david.patrones.clase.estructurales.adapter.guru;
 
 // Implementación de la biblioteca de análisis
-public class JsonAnalysisLibrary implements AnalysisLibrary {
+public class JsonAnalysisLibrary {
     private String jsonData;
 
     public JsonAnalysisLibrary(String jsonData) {
         this.jsonData = jsonData;
     }
 
-    @Override
     public void analyzeData() {
         System.out.println("Analizando datos JSON: " + jsonData);
     }
